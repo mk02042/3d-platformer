f3bf441819981693eaa32f9c84ffec4eb6e69eef
milk20090 <alhahay0603@gmail.com>
2026-04-25 20:20:10 +0800

    ш

---
+++ Assets/Level_2/code/temperstura.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class temperstura : MonoBehaviour
+{
+    public Health health;
+    public int playerDamage = 2;
+    public float temperatureCurrent = 36.6f;
+    public float temperatureNormal = 36.6f;
+    public float temperatureCritical = 34f;
+    public float freezeSpeed = 0.65f;
+
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
